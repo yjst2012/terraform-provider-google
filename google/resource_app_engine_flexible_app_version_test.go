99f1e2f8f1240be1c385bdf2923a6356aa9d3927
Megan Bang <mbang@hashicorp.com>
2020-03-24 10:50:34 -0500

    remove provisioner

--- google/resource_app_engine_flexible_app_version_test.go
+++ google/resource_app_engine_flexible_app_version_test.go
@@ -50,9 +50,6 @@ resource "google_project" "my_project" {
   project_id      = "%{project}"
   org_id          = "%{org_id}"
   billing_account = "%{billing_acct}"
-  provisioner "local-exec" {
-      command = "sleep 10"
-  }
 }
 
 resource "google_app_engine_flexible_app_version" "foo" {
@@ -147,9 +144,6 @@ resource "google_project" "my_project" {
   project_id      = "%{project}"
   org_id          = "%{org_id}"
   billing_account = "%{billing_acct}"
-  provisioner "local-exec" {
-      command = "sleep 10"
-  }
 }
 
 resource "google_app_engine_flexible_app_version" "foo" {
